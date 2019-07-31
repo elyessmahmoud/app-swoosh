86467855ca23e893dc6d3cbf83b967eb2d365bad
Elyess Mahmoud <elyessmahmoud@Elyesss-MacBook-Pro.local>
2019-07-31 09:17:51 +0100

    minor changes

---
+++ app-swoosh/app-swoosh/ViewController.swift
@@ -0,0 +1,24 @@
+//
+//  ViewController.swift
+//  app-swoosh
+//
+//  Created by Elyess Mahmoud on 7/29/19.
+//  Copyright © 2019 Elyess Mahmoud. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+   
+    @IBOutlet weak var swoosh: UIImageView!
+    @IBOutlet weak var bgImg: UIImageView!
+    
+    override func viewDidLoad() {
+    
+        
+    }
+
+
+}
+
